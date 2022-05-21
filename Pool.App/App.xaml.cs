98efca5f824f06a773408e0c8c965255eeed178f
krzm <kmazanek@gmail.com>
2022-05-21 22:37:42 +0200

    Add refactored app proj

---
+++ Pool.App/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows;
+using Pool.Container;
+using Unity;
+
+namespace Pool.App;
+
+public partial class App
+	: Application
+{
+	private void ApplicationStartup(object sender, StartupEventArgs e)
+	{
+		BuildByContainer();
+		//BuildByFactory();
+	}
+
+	private void BuildByFactory()
+	{
+		var gameFactory = new GameFactory();
+		var gameView = gameFactory.Order();
+		MainWindow = gameView as Window;
+		MainWindow.Show();
+	}
+
+	private void BuildByContainer()
+	{
+		var container = new UnityContainer();
+		var gameContainer = new GameContainer(container);
+		var gameView = gameContainer.Order();
+		MainWindow = gameView as Window;
+		MainWindow.Show();
+	}
+}
---
+++ Pool.App/MainWindow.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace Pool.App;
+
+public partial class MainWindow : Window
+{
+	public MainWindow()
+	{
+		InitializeComponent();
+	}
+}
